787491375a3ed8fc0867b164af467984b0ce0fa7
Rataslow <72532384+Rataslow@users.noreply.github.com>
2024-09-04 11:11:31 -0400

    Add files via upload

---
+++ js/Denuncia.js
@@ -0,0 +1,12 @@
+function ir(){
+
+
+if(document.getElementById("comentario").value){
+    window.location="Denuncia.html";
+    alert("Los datos se modificaron con exito");}
+    
+
+else{
+alert("Complete todos los items");
+}
+}
---
+++ js/log.js
@@ -0,0 +1,14 @@
+function ir(){
+
+    var c= 1234;
+    var u="daniel";
+	var p=1234;
+
+if(document.form.password.value==c && document.form.login.value==u && document.form.poblador.value==p){
+    alert("Esta ingresando a realizar una denuncia");
+    window.location="Denuncia.html";}
+
+else{
+alert("Ingrese los datos correctamente");
+}
+}
